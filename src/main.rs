115008fff6cefedc8bccd6359897c4d09b9d10f2
Mytyl <github.aeyip@simplelogin.com>
2025-03-04 01:27:26 -0500

    tested simd

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#![feature(portable_simd)]
+
+use std::simd::{f32x8, StdFloat};
+
 use macroquad::prelude::*;
-use miniquad::conf::Platform;
 use raylib_rs::{
     clamp, player::Player, traits::Drawable, Bullet, PLAYER_SIZE, PLAYER_SLOW, PLAYER_SPEED,
     PROJECTILE_CAP, SCREEN_H, SCREEN_W,
@@ -18,10 +21,6 @@ fn conf() -> Conf {
         window_height: SCREEN_H as i32,
         window_width: SCREEN_W as i32,
         window_resizable: false,
-        platform: Platform {
-            swap_interval: Some(60),
-            ..Default::default()
-        },
         ..Default::default()
     }
 }
@@ -30,13 +29,44 @@ fn conf() -> Conf {
 async fn main() {
     let mut player = Player::new();
 
+    let mut time = 0.0;
+
+    let mut test_particles_x = vec![0.0; 20];
+    let mut test_particles_y = (0..20).map(|i| 50.0 * i as f32).collect::<Vec<f32>>();
+
     loop {
         clear_background(WHITE);
         // TODO: draw fps
-        draw_text(format!("a"), x, y, font_size, color);
 
         player.draw();
+
+        let chunk_size = 8;
+        let mut i = 0;
+
+        while i + chunk_size <= 20 {
+            let x = f32x8::from_slice(&test_particles_x[i..chunk_size + i]);
+            let new_x = x + f32x8::splat(2.0);
+            let sine = (x + f32x8::splat(time)).sin();
+
+            test_particles_x[i..chunk_size + i].copy_from_slice(&new_x.to_array());
+            test_particles_y[i..chunk_size + i].copy_from_slice(&sine.to_array());
+
+            i += chunk_size;
+        }
+
+        // stragglers
+        while i < 20 {
+            test_particles_x[i] += 2.0;
+            test_particles_y[i] = (test_particles_x[i] + time).sin();
+            i += 1
+        }
+
+        for p in 0..20 {
+            draw_circle(test_particles_x[p], test_particles_y[p], 30.0, PINK);
+        }
+
         handle_input(&mut player.pos);
+        time += 0.2;
 
         next_frame().await;
     }
